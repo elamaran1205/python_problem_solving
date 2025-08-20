f9431dba2e5c60f21b1015fc9cc2ec87f21f19aa
elamaran <elamara1205@gmail.com>
2025-08-20 20:03:58 +0530

    problem04 with fixes

--- problem04_shopping_cart_total.py
+++ problem04_shopping_cart_total.py
@@ -1,14 +1,24 @@
+# Problem 04: Shopping Cart System
+
+# Description:
+# - Created a shopping cart program in Python.
+# - User can add items with price.
+# - Program shows total items, each item with price
+
+
 import sys
 cart = {}
 print("Welcome to LMN shopping cart section")
 
-def total_amount():
+def total_cart():
+    print(f"Total items in cart: {len(cart.items())}")
     for itemm,prices in cart.items():
         print(f"{itemm} - ₹{prices}")
     total = sum(cart.values())
     large = max(cart,key=cart.get)
     print(f"Highest amount item : {large} - Price(₹{cart[large]})")
-    sys,exit(f"Total Amount : {total}")
+    print(f"Total Amount : {total}")
+    check_input()
 
 
 def add_items():
@@ -16,16 +26,15 @@ def add_items():
     items = input("Enter the item name:")
     price = int(input("Enter the price of the item without currency symbol:"))
     cart[items] = price
-    for itemm,prices in cart.items():
-         print(f"{itemm} - ₹{prices}")
-         check=int(input("To see cart press 1 | Skip? Press 0 to continue:"))
-         if check == 1:
-             total_amount()
-         elif check == 0:
-            check_input()
-         else:
-             print("Invalid input")
-             check_input()
+    print(f"{items} - ₹{price}")
+    check=int(input("To see cart press 1 | Skip? Press 0 to continue:"))
+    if check == 1:
+        total_cart()
+    elif check == 0:
+        check_input()
+    else:
+        print("Invalid input")
+        check_input()
                      
                 
 def check_input():
